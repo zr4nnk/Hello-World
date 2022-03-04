a53fa9c24f826fbcb755d616cadb0a179e2275d3
zr4nnk <100956161+zr4nnk@users.noreply.github.com>
2022-03-04 22:16:57 +0530

    hello world code
    java file hello world

---
+++ Hello.java
@@ -0,0 +1,12 @@
+
+public class Hello {
+void display()
+{
+	System.out.println("This is Hello World Basic Program. ");
+}
+	public static void main(String[] args) {
+Hello h=new Hello();
+h.display();
+	}
+
+}
